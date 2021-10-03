b729469c79e1f55c44eb7cf6ca6321f7b5369008
Masa <masa@hashicorp.com>
2021-10-03 07:22:53 +0000

    Added vnet module

--- network.tf
+++ network.tf
@@ -2,6 +2,6 @@ module "network" {
   source  = "app.terraform.io/masa-training/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-  ressource_group_name = azurerm_resource_group.myresourcegroup.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
 
