ce423e809a4ad95ca668ec1b20f14e5c5b19d1f5
Masatomo Ito <masa@hashicorp.com>
2021-10-06 03:14:47 +0000

    test

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -12,7 +12,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. うごけー！ 
+  ようこそ ${PREFIX}のヒゲおじさんのアプリケーションへ！
   <!-- END -->
   
   </div>
